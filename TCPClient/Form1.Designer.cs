31290ff21985e9ffe5abb3e5138d80b71866f5ff
Isaac Anazagasty <isgam96@gmail.com>
2019-10-19 11:27:41 -0400

    Add project files.

---
+++ TCPClient/Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace TCPClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtsend = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.BtnEnviar = new System.Windows.Forms.Button();
+            this.BtnConectar = new System.Windows.Forms.Button();
+            this.TxtIPServer = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.LblEstado = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.TxtPuerto = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timerBuscarMensajes = new System.Windows.Forms.Timer(this.components);
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtsend
+            // 
+            this.txtsend.Location = new System.Drawing.Point(13, 108);
+            this.txtsend.Multiline = true;
+            this.txtsend.Name = "txtsend";
+            this.txtsend.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtsend.Size = new System.Drawing.Size(336, 90);
+            this.txtsend.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 272);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(254, 119);
+            this.textBox2.TabIndex = 1;
+            // 
+            // BtnEnviar
+            // 
+            this.BtnEnviar.Location = new System.Drawing.Point(364, 108);
+            this.BtnEnviar.Name = "BtnEnviar";
+            this.BtnEnviar.Size = new System.Drawing.Size(75, 23);
+            this.BtnEnviar.TabIndex = 3;
+            this.BtnEnviar.Text = "Send";
+            this.BtnEnviar.UseVisualStyleBackColor = true;
+            this.BtnEnviar.Click += new System.EventHandler(this.BtnEnviar_Click);
+            // 
+            // BtnConectar
+            // 
+            this.BtnConectar.Location = new System.Drawing.Point(297, 40);
+            this.BtnConectar.Name = "BtnConectar";
+            this.BtnConectar.Size = new System.Drawing.Size(75, 23);
+            this.BtnConectar.TabIndex = 5;
+            this.BtnConectar.Text = "Connect";
+            this.BtnConectar.UseVisualStyleBackColor = true;
+            this.BtnConectar.Click += new System.EventHandler(this.BtnConectar_Click);
+            // 
+            // TxtIPServer
+            // 
+            this.TxtIPServer.Location = new System.Drawing.Point(16, 40);
+            this.TxtIPServer.Name = "TxtIPServer";
+            this.TxtIPServer.Size = new System.Drawing.Size(147, 20);
+            this.TxtIPServer.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(16, 21);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(76, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "IP del Servidor";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(13, 89);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(88, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Escriba Mensaje:";
+            // 
+            // LblEstado
+            // 
+            this.LblEstado.AutoSize = true;
+            this.LblEstado.Location = new System.Drawing.Point(393, 46);
+            this.LblEstado.Name = "LblEstado";
+            this.LblEstado.Size = new System.Drawing.Size(75, 13);
+            this.LblEstado.TabIndex = 13;
+            this.LblEstado.Text = "No conectado";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(396, 21);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(43, 13);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Estado:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(19, 253);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(100, 13);
+            this.label9.TabIndex = 15;
+            this.label9.Text = "Mensajes recibidos:";
+            // 
+            // TxtPuerto
+            // 
+            this.TxtPuerto.Location = new System.Drawing.Point(188, 40);
+            this.TxtPuerto.Name = "TxtPuerto";
+            this.TxtPuerto.Size = new System.Drawing.Size(78, 20);
+            this.TxtPuerto.TabIndex = 16;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(185, 21);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(97, 13);
+            this.label10.TabIndex = 17;
+            this.label10.Text = "Puerto del Servidor";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pictureBox1.Location = new System.Drawing.Point(290, 269);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(149, 122);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 19;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // timerBuscarMensajes
+            // 
+            this.timerBuscarMensajes.Enabled = true;
+            this.timerBuscarMensajes.Interval = 1000;
+            this.timerBuscarMensajes.Tick += new System.EventHandler(this.timerBuscarMensajes_Tick);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "select archive";
+            this.openFileDialog1.Filter = "Files|*.jpg;*.jpeg;*.png;*.pdf;*.xls;*.mp4;*.mp3;";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(487, 419);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.TxtPuerto);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.LblEstado);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.TxtIPServer);
+            this.Controls.Add(this.BtnConectar);
+            this.Controls.Add(this.BtnEnviar);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.txtsend);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtsend;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button BtnEnviar;
+        private System.Windows.Forms.Button BtnConectar;
+        private System.Windows.Forms.TextBox TxtIPServer;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label LblEstado;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox TxtPuerto;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timerBuscarMensajes;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ TCPClient/Form1.cs
@@ -0,0 +1,296 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//ADDED REFERENCE LIBRARIES
+using System.Net.Sockets;          //Network communication sockets for TCP/IP
+//using System.Text;                 //Receive and send InfoBytes. Dats bytes
+using System.Net.NetworkInformation;
+//using System;
+using System.IO;
+using System.Net;
+using System.Drawing.Imaging;
+
+namespace TCPClient
+{
+    public partial class Form1 : Form
+    {
+        //G:OBALES
+
+        TcpClient TCPCliente;
+        int iPuerto = 9999;
+        string fileSelected = "";
+        private const int BufferSize = 1024;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void BtnConectar_Click(object sender, EventArgs e)
+        {
+            IPAddress address;
+            string sIP = "";
+            
+            if (TxtIPServer.Text == "")
+            {
+                sIP = "10.0.0.2";
+                TxtIPServer.Text = sIP;
+
+            } else if (IPAddress.TryParse(TxtIPServer.Text, out address))
+            {
+                sIP = address.ToString();
+            } else
+            {
+                sIP = "10.0.0.2";
+                TxtIPServer.Text = sIP;
+            }
+
+            if (TxtPuerto.Text == "" || !Int32.TryParse(TxtPuerto.Text, out iPuerto))
+            {
+                iPuerto = 1220;
+            }
+
+            if (iPuerto >= 65535 && iPuerto <= 1024)
+            {
+                iPuerto = 1220;
+            }
+
+            try
+            {
+                //Conectar al Servior
+                TCPCliente = new TcpClient(sIP, iPuerto);
+               //TCPCliente.Connect(sIP, iPuerto); //connect to server
+
+                //
+                if (TCPCliente.Connected)
+                {
+                    LblEstado.Text = "Conectado";
+                }
+            }
+            catch(Exception error){
+                Console.WriteLine("Error => " + error.ToString());
+                MessageBoxButtons buttons = MessageBoxButtons.OK;
+                MessageBox.Show("No se pudo conectar al servidor, intentelo màs tarde.", "Error de Conexiòn",buttons);
+            }
+        }
+
+        public String VerSiHayMensajes()
+        {
+            String Mensaje = "";
+            try {
+                System.Net.Sockets.NetworkStream stream = TCPCliente.GetStream();
+
+                //Data reciving mechanism
+                //Corrigue este metodo para que siempre este en escuchar y cuando llegue un mensaje lo puedas ver inmediatamente.
+                if (stream.CanRead) //Preguntamos si es posible leer
+                {
+                    if (stream.DataAvailable) //Preguntamos si hay algo por leer
+                    {
+                        Byte[] data = new Byte[TCPCliente.ReceiveBufferSize];       //Buffer del tamaño de lo que se recibio
+                        string responseData = "";  //String to store the response ASCII representation
+
+                        Int32 bytes = stream.Read(data, 0, data.Length);
+                        responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
+                        //concatenamos los bytes en la variable mensaje para armar el mensaje
+                        Mensaje += "Received " + responseData;
+                    }
+                }
+            }
+            catch (Exception ex){
+                //ex nunca se usa.. contiene el errorde que no hay mensajes de clientes ya que no se eonctraron clientes.
+                //no hay clientes conectados al servidor, asi que no hacemos nada.
+            }
+            //regresamos el mensaje que se construyo anteriormente
+            return Mensaje;
+
+
+        }
+
+        private void BtnRecibir_Click(object sender, EventArgs e)
+        {
+            System.Net.Sockets.NetworkStream stream = TCPCliente.GetStream();
+
+            //Data reciving mechanism
+			//Corrigue este metodo para que siempre este en escuchar y cuando llegue un mensaje lo puedas ver inmediatamente.
+            if (stream.CanRead) //Preguntamos si es posible leer
+            {
+                if (stream.DataAvailable) //Preguntamos si hay algo por leer
+                {
+                    Byte[] data = new Byte[TCPCliente.ReceiveBufferSize];       //Buffer del tamaño de lo que se recibio
+                    string responseData = "";  //String to store the response ASCII representation
+
+                    Int32 bytes = stream.Read(data, 0, data.Length);
+                    responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
+                    textBox2.Text += "Received " + responseData;
+                }
+            }
+        }
+
+        private void BtnEnviar_Click(object sender, EventArgs e)
+        {
+            
+
+            if (txtsend.Text.Trim() != "")
+            {
+                try
+                {
+                    // Data sending mechanism
+                    //local constructor
+                    System.Net.Sockets.NetworkStream InfoBytes;
+                    InfoBytes = TCPCliente.GetStream();
+                    Byte[] TempOutStringData; // = new Byte();
+                    TempOutStringData = System.Text.Encoding.ASCII.GetBytes(txtsend.Text + "\r\n");
+                    InfoBytes.Write(TempOutStringData, 0, TempOutStringData.Length);
+                    //Borra el contenido de textbox
+                    txtsend.Text = "";
+                }
+                catch (Exception error)
+                {
+                    Console.WriteLine("Error => " + error.ToString());
+                }
+            }  
+
+
+            //enviar imagen
+            try
+            {
+                byte[] SendingBuffer = null;
+                System.Net.Sockets.NetworkStream netstream;
+                netstream = TCPCliente.GetStream();
+                FileStream Fs = new FileStream(fileSelected, FileMode.Open, FileAccess.Read);
+                int NoOfPackets = Convert.ToInt32
+             (Math.Ceiling(Convert.ToDouble(Fs.Length) / Convert.ToDouble(BufferSize)));
+                int TotalLength = (int)Fs.Length, CurrentPacketLength, counter = 0;
+                for (int i = 0; i < NoOfPackets; i++)
+                {
+
+                    CurrentPacketLength = TotalLength;
+                    SendingBuffer = new byte[CurrentPacketLength];
+                    Fs.Read(SendingBuffer, 0, CurrentPacketLength);
+                    netstream.Write(SendingBuffer, 0, (int)SendingBuffer.Length);
+
+                }
+                Fs.Close();
+                netstream.Flush();
+                pictureBox1.Image = null;
+
+            }
+            catch (Exception ex) {
+                MessageBox.Show(ex.ToString());
+            }
+        
+        }
+
+        //manda esta variable como cualquier texto
+        string base64 = null;
+        private void sendImg_Click(object sender, EventArgs e)
+        {
+            //char[] delimiter = image_splitter.ToCharArray();
+            var fileContent = string.Empty;
+            var filePath = string.Empty;
+            //Abre un cuadro de dialogo para seleccionar una imagen
+            using (OpenFileDialog openFile = new OpenFileDialog()) {
+                openFile.InitialDirectory = "../Imágen";
+                openFile.Filter = "images Files (*.jpg)|*.jpg|All Files (*.*)|*.*";
+                openFile.FilterIndex = 2;
+                openFile.RestoreDirectory = true;
+
+                if (openFile.ShowDialog() == DialogResult.OK) {
+                    //Get the path of specified file
+                    filePath = openFile.FileName;
+
+                    
+                    using (Bitmap bmp = new Bitmap(openFile.FileName))
+                    {
+                        using (MemoryStream ms = new MemoryStream())
+                        {
+                            bmp.Save(ms, ImageFormat.Jpeg);
+                            base64 = Convert.ToBase64String(ms.ToArray());
+                        }
+                    }
+                }
+               //esto ira en el server
+                using (MemoryStream mss = new MemoryStream(Convert.FromBase64String(base64)))
+                {
+                    pictureBox1.Image = Image.FromStream(mss);
+                    pictureBox1.SizeMode= PictureBoxSizeMode.StretchImage;
+                }
+                MessageBox.Show(base64);
+                
+            }
+
+            
+        }
+
+        private void timerBuscarMensajes_Tick(object sender, EventArgs e)
+        {
+            //llamamos a la funcion VerSiHayMensajes que nos retorna una cadena con el ultimo mensaje enviado por el servidor
+            //
+            String mensaje = VerSiHayMensajes();
+            //obtenermos el texto actual que tenemos en los mensajes recibidos por el servidor
+            String textoActual = textBox2.Text;
+            //si hay mensaje recibido por el servidor
+            if (mensaje.Trim() != "")
+            {
+                //y si NO es la primer vez que el servidor nos envia mensaje
+                if (textoActual != "") {
+                    //agregamos un salto de linea para mostrar el nuevo mensaje recibido en linea nueva
+                    textBox2.AppendText(Environment.NewLine);
+                }
+                //escribimos en la nueva linea el mensaje recibido
+                textBox2.AppendText(mensaje.Trim());
+            }
+
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() != DialogResult.OK)
+
+                return;
+
+            System.IO.FileInfo fInfo = new System.IO.FileInfo(openFileDialog1.FileName);
+
+            string strFileName = fInfo.Name;
+
+            string strFilePath = fInfo.DirectoryName;
+
+            fileSelected = strFilePath + "\\" + strFileName;
+            string ext = Path.GetExtension(fileSelected);
+
+            byte[] send = File.ReadAllBytes(fileSelected);
+            switch (ext) { 
+                case ".jpg":
+                        System.Drawing.Image img =
+              System.Drawing.Image.FromFile(fileSelected, true);
+                        pictureBox1.Image = img;
+                break;
+                case ".png":
+                    System.Drawing.Image png =
+          System.Drawing.Image.FromFile(fileSelected, true);
+                    pictureBox1.Image = png;
+                    break;
+                case ".pdf":
+                    pictureBox1.Image = TCPClient.Properties.Resources.pdfIcon.ToBitmap();
+                    break;
+            }
+
+        }
+
+      
+    }
+}
